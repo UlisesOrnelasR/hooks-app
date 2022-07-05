345d7aa9defb44c4a2afb5eea97ea94c0f00b7fa
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-05 11:10:25 -0600

    useEffect

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,14 @@
+export const SimpleForm = () => {
+  return (
+    <>
+      <h1>Formulario simple</h1>
+      <hr />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="username"
+        name="username"
+      />
+    </>
+  );
+};
