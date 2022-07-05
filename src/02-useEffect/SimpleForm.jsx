f3246bda8e2db56b345b8a6bd6be5d86bd740c12
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-05 11:34:35 -0600

    Ejercicio con use Effect

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,4 +1,33 @@
+import { useState, useEffect } from "react";
+
 export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "ulises",
+    email: "ulfiornelasro@ittepic.edu.mx",
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    console.log("useEffect called");
+  }, []);
+
+  useEffect(() => {
+    console.log("form state changed");
+  }, [formState]);
+
+  useEffect(() => {
+    console.log("emailllllchanged");
+  }, [email]);
+
   return (
     <>
       <h1>Formulario simple</h1>
@@ -8,6 +37,17 @@ export const SimpleForm = () => {
         className="form-control"
         placeholder="username"
         name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="ornelas@gmail.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
       />
     </>
   );
