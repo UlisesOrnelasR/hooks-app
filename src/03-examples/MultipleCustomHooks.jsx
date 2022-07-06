8ffacb900477c713c4e30ba3d62d3743c94ddbc2
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-05 23:25:02 -0600

    Añadiendo estilo con bootstrap para visualizar mejor el ejercicio

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,11 +4,16 @@ export const MultipleCustomHooks = () => {
   const { data, isLoading, hasError } = useFetch(
     "https://www.breakingbadapi.com/api/quotes/1"
   );
-  console.log(data, isLoading, hasError);
+
   return (
     <>
       <div>BreakingBad Quotes</div>
       <hr />
+      <div className="alert alert-info text-center">Loading...</div>
+      <blockquote className="blockquote text-end">
+        <p className="mb-2">Hola mundo</p>
+        <footer className="blockquote-footer">Ulises Ornelas</footer>
+      </blockquote>
     </>
   );
 };
