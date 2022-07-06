9f95e9b24a1e75cb60e296699acfcce4843b9a67
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-06 10:09:12 -0600

    conectando custom hooks, useCounter y useFetch

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,14 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
+  const { counter, increment } = useCounter(1);
+
   const { data, isLoading, hasError } = useFetch(
-    "https://www.breakingbadapi.com/api/quotes/1"
+    `https://www.breakingbadapi.com/api/quotes/${counter}`
   );
 
+  const { author, quote } = !!data && data[0];
   return (
     <>
       <div>BreakingBad Quotes</div>
@@ -13,10 +17,14 @@ export const MultipleCustomHooks = () => {
         <div className="alert alert-info text-center">Loading...</div>
       ) : (
         <blockquote className="blockquote text-end">
-          <p className="mb-2">Hola mundo</p>
-          <footer className="blockquote-footer">Ulises Ornelas</footer>
+          <p className="mb-2">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
         </blockquote>
       )}
+
+      <button className="btn btn-primary" onClick={() => increment(1)}>
+        Next quote
+      </button>
     </>
   );
 };
