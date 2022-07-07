8abf42b523382ed80ef5d6038b318104f58c655c
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-06 22:53:16 -0600

    Dandole estilo con Bootstrap a TodoApp

---
+++ src/08-useReducer/todoReducer.js
@@ -0,0 +1,15 @@
+
+
+export const todoReducer = (initialState=[],action) => {
+  
+    switch (action.type) {
+        case 'ABC':
+            throw new console.error();
+            
+    
+        default:
+            return initialState
+    }
+
+
+}
