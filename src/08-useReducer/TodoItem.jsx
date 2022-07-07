6def9d45a7bea8c92f8a998a5aa842a359696a69
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-07 09:51:05 -0600

    Añadiendo key en TodoItem

--- src/08-useReducer/TodoItem.jsx
+++ src/08-useReducer/TodoItem.jsx
@@ -1,9 +1,6 @@
 export const TodoItem = ({ todo }) => {
   return (
-    <li
-      key={todo.id}
-      className="list-group-item d-flex justify-content-between"
-    >
+    <li className="list-group-item d-flex justify-content-between">
       <span className="align-self-center">{todo.description}</span>
       <button className="btn btn-danger">Borrar</button>
     </li>
--- src/08-useReducer/TodoList.jsx
+++ src/08-useReducer/TodoList.jsx
@@ -5,7 +5,7 @@ export const TodoList = ({ todos }) => {
   return (
     <ul className="list-group">
       {todos.map((todo) => (
-        <TodoItem todo={todo} />
+        <TodoItem todo={todo} key={todo.id} />
       ))}
     </ul>
   );
