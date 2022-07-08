f12f953a5d3ce558aa3c8f00aea991c56d27cfa7
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-08 11:44:02 -0600

    Refactorizando router en un componente Navbar

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,21 @@
+import { HomePage } from "./HomePage";
+import { AboutPage } from "./AboutPage";
+import { LoginPage } from "./LoginPage";
+import { Navbar } from "./Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <Navbar />
       <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
     </>
   );
 };
